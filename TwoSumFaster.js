0bf4265e279cd057f1f42abff391aee39b57120c
lutaoact <i@lutao.me>
2017-06-01 19:55:40 +0800

    TwoSumFaster.js

---
+++ TwoSumFaster.js
@@ -0,0 +1,22 @@
+'use strict';
+
+let a = [-18, -15, -12, -7, 0, 0, 2, 3, 12, 15, 15, 17, 18];
+console.log(TwoSumFaster(a));
+
+function TwoSumFaster(a) {
+  let low = 0, high = a.length - 1, count = 0;
+  while (low < high) {
+    let sum = a[low] + a[high];
+    console.log(low, high, sum);
+    if (sum === 0) {
+      count++;
+      high--;
+      low++;
+    } else if (sum < 0) {
+      low++;
+    } else {
+      high--;
+    }
+  }
+  return count;
+}
