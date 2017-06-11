4cd10a08556858fd88c10b44d38db1b5ce92e304
lutaoact <i@lutao.me>
2017-06-11 17:43:20 +0800

    2.1.11.shellSort.js

---
+++ 2.1.11.shellSort.js
@@ -0,0 +1,29 @@
+'use strict';
+
+let a;
+a = ['E', 'A', 'S', 'Y', 'S', 'H', 'E', 'L', 'L', 'S', 'O', 'R', 'T', 'Q', 'U', 'E', 'S', 'T', 'I', 'O', 'N'];
+
+console.log(shellSort(a));
+
+function shellSort(a) {
+  let N = a.length, h = 1;
+  let hlist = [1];
+  while (h < ~~(N / 3)) hlist.unshift(h = h * 3 + 1);
+  console.log(hlist);
+
+  hlist.forEach(function(h) {
+    for (let i = h; i < N; i++) {
+      for (let j = i; j >= h && a[j - h] > a[j]; j -= h) {
+        swap(a, j, j - h);
+      }
+    }
+    h = ~~(h / 3);
+  });
+  return a;
+}
+
+function swap(a, i, j) {
+  let temp = a[i];
+  a[i] = a[j];
+  a[j] = temp;
+}
