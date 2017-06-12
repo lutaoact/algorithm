83516452aeafa7e53fddf0f0ec81331b5057e5cf
lutaoact <i@lutao.me>
2017-06-12 20:42:28 +0800

    powerOfTwo.js

---
+++ powerOfTwo.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/*
+231. Power of Two
+
+Given an integer, write a function to determine if it is a power of two.
+*/
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function(n) {
+  return n > 0 && (n & (n - 1)) === 0;
+};
