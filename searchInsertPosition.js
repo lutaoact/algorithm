2e0dcde99c15f81edfd12f0cc10598954d9c4ec5
lutaoact <i@lutao.me>
2017-06-13 10:58:56 +0800

    searchInsertPosition.js

---
+++ searchInsertPosition.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/*
+35. Search Insert Position
+
+Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You may assume no duplicates in the array.
+
+Here are few examples.
+[1,3,5,6], 5 → 2
+[1,3,5,6], 2 → 1
+[1,3,5,6], 7 → 4
+[1,3,5,6], 0 → 0
+*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+  let N = nums.length, lo = 0, hi = N - 1, mid;
+  if (target <= nums[lo]) return 0;
+  if (target > nums[hi]) return N;
+
+  while (lo <= hi) {
+    mid = ~~((lo + hi) / 2);
+    if (nums[mid] < target) {
+      lo = mid + 1;
+    } else if (nums[mid] > target) {
+      hi = mid - 1;
+    } else {
+      return mid;
+    }
+  }
+  return lo;
+};
+
+console.log(searchInsert([1, 3, 5, 6], 4));
