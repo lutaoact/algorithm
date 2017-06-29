95a57e0d09b84a8d9064843afa98a5e0a1f96ffd
lutaoact <i@lutao.me>
2017-06-29 17:41:57 +0800

    530.MinimumAbsoluteDifferenceInBST.js

---
+++ 530.MinimumAbsoluteDifferenceInBST.js
@@ -0,0 +1,83 @@
+'use strict';
+
+/*
+530. Minimum Absolute Difference in BST
+Given a binary search tree with non-negative values, find the minimum absolute difference between values of any two nodes.
+Example:
+
+Input:
+
+   1
+    \
+     3
+    /
+   2
+
+Output:
+1
+
+Explanation:
+The minimum absolute difference is 1, which is the difference between 2 and 1 (or between 2 and 3).
+Note: There are at least two nodes in this BST.
+*/
+
+/**
+ * Definition for a binary tree node.
+ */
+function TreeNode(val) {
+    this.val = val;
+    this.left = this.right = null;
+}
+
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var getMinimumDifference = function(root) {
+  let stack = [], node = root, min = Infinity, prev = null;
+  stack.push(node);
+  while (node.left) {
+    node = node.left;
+    stack.push(node);
+  }
+
+  while (stack.length > 0) {
+    node = stack.pop();
+    if (prev !== null && node.val - prev < min) min = node.val - prev;
+    prev = node.val;
+    if (node.right) {
+      node = node.right;
+      stack.push(node);
+
+      while (node.left) {
+        node = node.left;
+        stack.push(node);
+      }
+    }
+  }
+  return min;
+};
+
+getMinimumDifference = function(root) {
+  let min = Infinity, prev = null;
+  function inOrder(node) {
+    if (node === null) return;
+    inOrder(node.left);
+    if (prev !== null && node.val - prev < min) min = node.val - prev;
+
+    prev = node.val;
+    inOrder(node.right);
+  }
+  inOrder(root);
+  return min;
+};
+
+let root = new TreeNode(4);
+root.left = new TreeNode(2);
+root.right = new TreeNode(7);
+root.left.left = new TreeNode(1);
+root.left.right = new TreeNode(3);
+root.right.left = new TreeNode(6);
+root.right.right = new TreeNode(9);
+
+console.log(getMinimumDifference(root));
