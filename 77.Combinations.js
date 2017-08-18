2c7e98c814d9364823b43d872c02f0e57fcdc16e
lutaoact <i@lutao.me>
2017-08-18 13:45:14 +0800

    77.Combinations.js

---
+++ 77.Combinations.js
@@ -0,0 +1,46 @@
+'use strict';
+
+/*
+77. Combinations
+
+Given two integers n and k, return all possible combinations of k numbers out of 1 ... n.
+
+For example,
+If n = 4 and k = 2, a solution is:
+
+[
+  [2,4],
+  [3,4],
+  [2,3],
+  [1,2],
+  [1,3],
+  [1,4],
+]
+*/
+
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number[][]}
+ */
+//回溯法的典型应用
+var combine = function(n, k) {
+  let tmpList = [], results = [];
+  backtrack(1, tmpList);
+  return results;
+
+  function backtrack(start, tmpList) {
+    if (tmpList.length === k) {
+      results.push(tmpList.slice());
+      return;
+    }
+
+    for (let i = start; i <= n; i++) {
+      tmpList.push(i);
+      backtrack(i + 1, tmpList);//元素不能重复，所以从i+1开始
+      tmpList.pop();
+    }
+  }
+};
+
+console.log(combine(4, 2));
