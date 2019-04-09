1cc3eeec21df1105b1aa8f6323d7ec13430d54ce
lutaoact <i@lutao.me>
2019-04-09 09:04:00 +0800

    1010.PairsOfSongsWithTotalDurationsDivisibleBy60.java

--- 1010.PairsOfSongsWithTotalDurationsDivisibleBy60.java
+++ 1010.PairsOfSongsWithTotalDurationsDivisibleBy60.java
@@ -14,10 +14,24 @@ class Solution {
         }
         return result;
     }
+    public int numPairsDivisibleBy60_2(int[] time) {
+        int[] counts = new int[60];
+        int result = 0;
+        for (int i = 0; i < time.length; i++) {
+            int remain = time[i] % 60;
+            result += counts[(60 - remain) % 60];
+            counts[remain]++;
+        }
+        return result;
+    }
     public static void main(String[] args) {
         Solution sol = new Solution();
         System.out.println(sol.numPairsDivisibleBy60(new int[]{30,20,150,100,40}));
         System.out.println(sol.numPairsDivisibleBy60(new int[]{60,60,60,60}));
         System.out.println(sol.numPairsDivisibleBy60(new int[]{30,30,30}));
+
+        System.out.println(sol.numPairsDivisibleBy60_2(new int[]{30,20,150,100,40}));
+        System.out.println(sol.numPairsDivisibleBy60_2(new int[]{60,60,60,60}));
+        System.out.println(sol.numPairsDivisibleBy60_2(new int[]{30,30,30}));
     }
 }
--- binarySearch.js
+++ binarySearch.js
@@ -25,3 +25,16 @@ function binarySearchLocal(a, key, low, high) {
   }
   return -1;
 }
+
+// 还要考虑重复值
+function binarySearchToGetLeastIndex(nums, key) {// 找出值大于等于key的最小索引
+  let low = 0, high = nums.length - 1;
+  while (low <= high) {
+    let mid = parseInt((low + high) / 2);
+    if (nums[mid] === key) return mid;
+
+    if (nums[mid] > key) high = mid - 1;
+    else if (nums[mid] < key) low = mid + 1;
+  }
+  return low;
+}
